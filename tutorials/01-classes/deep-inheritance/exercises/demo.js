fa3c0bc0c80b8775d263da0e0668934f3f45382e
nsok1007 <nsok1007@gmail.com>
2026-06-18 18:15:29 -0500

    finished demo

--- tutorials/01-classes/deep-inheritance/exercises/demo.js
+++ tutorials/01-classes/deep-inheritance/exercises/demo.js
@@ -22,12 +22,24 @@ class Wizard extends BaseCharacter {
 
 // DEMO: Create a FireWizard class that inherits from both Wizard and BaseCharacter
 // Add a fireDamage property and a castInferno() method
+class FireWizard extends Wizard {
+	constructor(characterName, startingMagic, baseFireDamage) {
+		super(characterName,startingMagic);
+		this.fireDamage = baseFireDamage;
+	}
 
+	castInferno() {
+		this.magicPoints -= 20;
+		return `${this.name} casts Inferno for ${this.fireDamage} fire damage! Magic left: ${this.magicPoints}.`;
+	}
+}
 // DEMO: Instantiate the grandchild element
+const ignis = new FireWizard("Ignis", 80, 45);
 
 // DEMO: Log a statement with properties from different class definitions in the
 // family tree, then call castInferno() and print the result.
-
+console.log(`${ignis.name} starts with ${ignis.health} total health and ${ignis.magicPoints} magic points.`);
+console.log(ignis.castInferno());
 /* 
     Follow up with additional exercises after demo for hands-on practice 
     with problem-solving and coding!
