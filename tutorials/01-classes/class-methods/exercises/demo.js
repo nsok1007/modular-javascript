84d619fb9008eed1b1b166ddf473b6508398a466
nsok1007 <nsok1007@gmail.com>
2026-06-18 18:02:58 -0500

    demo done

--- tutorials/01-classes/class-methods/exercises/demo.js
+++ tutorials/01-classes/class-methods/exercises/demo.js
@@ -7,9 +7,20 @@ class Wizard {
 		this.name = wizardName;
 		this.magicPoints = startingMagic;
 		// DEMO: Define an anonymous method that will be stored with every object instance
+		this.rest = () => {
+			this.magicPoints += 10;
+		} //stores the ENTIRE function
 	}
 
 	// DEMO: Define a named function (but omit keyword) that will be stored with the class
+	castSpell(spellName, cost) {
+		if(this.magicPoints >= cost) {
+			this.magicPoints -= cost;
+			return `${this.name} casts ${spellName}! Magic remaining: ${this.magicPoints}.`;
+		} else {
+			return `${this.name} does not have enough magic to cast ${spellName}!`;
+		}
+	} //stored at the CLASS LEVEL
 }
 
 /** EXAMPLE 2 - RUNNING REUSABLE ACTIONS **/
@@ -17,7 +28,8 @@ const player1 = new Wizard("Merlin", 60);
 const player2 = new Wizard("Gandalf", 15);
 
 // DEMO: Have each player cast a spell
-
+console.log(player1.castSpell("Fireball", 40));
+console.log(player2.castSpell("Lightning Bolt", 30));
 /* 
     Follow up with additional exercises after demo for hands-on practice 
     with problem-solving and coding!
