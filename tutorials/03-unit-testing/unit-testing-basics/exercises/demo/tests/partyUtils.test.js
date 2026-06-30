3253072f8d9f9dc008e7019ef079e8603f0c28cd
nsok1007 <nsok1007@gmail.com>
2026-06-30 14:11:49 -0500

    finished demo

--- tutorials/03-unit-testing/unit-testing-basics/exercises/demo/tests/partyUtils.test.js
+++ tutorials/03-unit-testing/unit-testing-basics/exercises/demo/tests/partyUtils.test.js
@@ -3,23 +3,29 @@
 // JEST TEST FILE: We import the functions we want to inspect
 import { calculateCombinedPower, getStarterSpells } from "../partyUtils.js";
 
-test.todo("calculateCombinedPower accurately sums combat values of party members");
+// // DEMO: Set up test with separate actual and expected variables
+test("calculateCombinedPower accurately sums combat values of party members", () => {
+    let actual = calculateCombinedPower(15,12); //gets stored as actual
+    let expected = 27; //usually a hard-coded arguement to test against
 
-// DEMO: Create isolated testing block
+    expect(actual).toBe(expected); //expect the actual to be the expected
 
-// DEMO: Set up test with separate actual and expected variables
+    // DEMO: Make assertion using variables
+    expect(calculateCombinedPower(0,50)).toBe(50); 
+    //npm test partyUtils.test.js --> run a file regardless of where you are in the directory
+}); 
 
-// DEMO: Make assertion using variables
+// test.todo("getStarterSpells returns the correct initial magic array layout"); //remove .todo to make it a test
+test("getStarterSpells returns the correct initial magic array layout", () => {
+    const spellbook = getStarterSpells();
 
-// DEMO Test again in a single line with new values
+    //.toBe and .toEqual matcher for data structure equality
+    expect(spellbook).not.toBe(["Fireball", "Heal Shield"]); 
+    expect(spellbook).toEqual(["Fireball", "Heal Shield"]); 
 
-test.todo("getStarterSpells returns the correct initial magic array layout");
-
-// DEMO Call function to be tested
-
-// DEMO: Show the difference between .toBe and .toEqual for data structure equality
-
-// DEMO: Confirm Fireball is included in array returned with .toContain
+    // .toContain matcher checks if the arguement is is included
+    expect(spellbook).toContain("Fireball");
+})
 
 /* 
     Follow up with additional exercises after demo for hands-on practice 
